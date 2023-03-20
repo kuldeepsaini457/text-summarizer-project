63d079dca31d77833ff0a033a11c2ffc4af1bd70
Ashutosh3817 <ashutosh.sinha1109@gmail.com>
2023-03-20 11:42:50 +0530

    initialize lstm

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.get_summary,name="GetSummary"),
+    path('abstractive',views.get_abstractive_summary,name="GetAbstractiveSummary"),
 ]
--- api/views.py
+++ api/views.py
@@ -21,3 +21,23 @@ def get_summary(request,*args,**kwargs):
 
     response={"title":title,"summary":summary.get('summary')}
     return Response(response)
+
+
+@api_view(['GET','POST'])
+def get_abstractive_summary(request,*args,**kwargs):
+   
+    """ title=request.data.get('title')
+    article=request.data.get('article')
+    lines=request.data.get('lines')
+
+    if lines is not None:
+        lines=int(lines)
+    if article is None:
+        return JsonResponse({"title":title,"article":article}) """
+
+    """ summarizer=Summarize(title,article,lines)
+    summary=summarizer.generate_summary() """
+
+
+    response={"title":"title","summary":"summary.get('summary')"}
+    return Response(response)
